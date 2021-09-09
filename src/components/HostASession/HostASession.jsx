d91e99821566c344dbb133857bdc4120316b1cff
Hassan Shahid <habibshahid2013@gmail.com>
2021-09-09 16:15:09 -0500

    implemented object into dispatch in compoenet

--- src/components/HostASession/HostASession.jsx
+++ src/components/HostASession/HostASession.jsx
@@ -4,6 +4,7 @@ import './host.css'
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
+import { useHistory } from 'react-router-dom';
 
 
 function HostASession(props) {
@@ -14,6 +15,32 @@ function HostASession(props) {
     const [time, setTime] = useState('')
     const [address, setAddress] = useState('')
     const [numberOfPeople, setNumberOfPeople] = useState(0)
+
+    //enables history to move between pages
+    const history = useHistory();
+
+    
+    const handleSession = (event) => {
+        event.preventDefault();
+        if (preferred_method.length > 0 && date.length > 0 && time.length > 0 && address.length > 0 && numberOfPeople.length > 0) {
+            //dispatch over to saga
+
+            dispatch({
+                type: '',
+                payload: {
+                    preferred_method: preferred_method,
+                    date: date,
+                    time: time,
+                    address: address,
+                    numberOfPeople: numberOfPeople,
+                }
+            })
+            history.push('/Dashboard')
+        }
+        else alert('Please put valid data into all of the input feilds')
+    
+    }
+   // end registerUser
 
     const useStyles = makeStyles((theme) => ({
         root: {
@@ -34,7 +61,8 @@ function HostASession(props) {
         <>
         <h1>Host a Meditation Session</h1>
             <div className={classes.root}>
-                <Grid container spacing={3}>
+                
+                <Grid container spacing={2} onSubmit={handleSession}>
                     <Grid item xs={12}>
                         <Paper className={classes.paper}> I am interested in hosting <select
                             type="text"
@@ -52,7 +80,7 @@ function HostASession(props) {
                             <option value="7">loving-kindness meditation session</option>
                             <option value="8">visualization meditation session</option>
                         </select>  
-                         _on
+                         during 
                         <input
                             type="date"
                             name="date" 
@@ -66,65 +94,57 @@ function HostASession(props) {
                          </Paper>
                 
                     </Grid>
-                    <Grid item xs={6}>
-                        <Paper className={classes.paper}>  
-                            <label for="time">Choose a time for your meeting:</label>
-                            <input
-                                type="time"
-                                id="time"
-                                name="time"
-                                min="06:00"
-                                max="24:00"
-                                value={time}
-                                onChange={(event) => setTime(event.target.value)}
-                                required>
-                            </input></Paper>
+                   
+                    <Grid item xs={12}>
+                            <Paper className={classes.paper}>
+                                <label for="time">Choose a time for your :</label>
+                                <input
+                                    type="time"
+                                    id="time"
+                                    name="time"
+                                    min="06:00"
+                                    max="24:00"
+                                    value={time}
+                                    onChange={(event) => setTime(event.target.value)}
+                                    required>
+                                </input></Paper>
                     </Grid>
-                    <Grid item xs={6}>
-                        <Paper className={classes.paper}> 
-                            <label for="time">In Person or Virtual: </label>
-                            <select
-                                type="test"
-                                name="location"
-                                value={address}
-                                onChange={(event) => setAddress(event.target.value)}
-                                required>
-                                <option defaultValue="">Choose one</option>
-                                <option value="1">In Person session</option>
-                                <option value="2">Virtual session</option>
+                    <Grid item xs={12}>
+                            <Paper className={classes.paper}>
+                                <label for="time">In Person or Virtual: </label>
+                                <select
+                                    type="test"
+                                    name="location"
+                                    value={address}
+                                    onChange={(event) => setAddress(event.target.value)}
+                                    required>
+                                    <option defaultValue="">Choose one</option>
+                                    <option value="1">In Person session</option>
+                                    <option value="2">Virtual session</option>
 
-                            </select></Paper>
-                        <Paper className={classes.paper}>
-                            <label for="time"> number of people able to attend: </label>
-                            <select
-                                type="number"
-                                name="number of people"
-                                value={numberOfPeople}
-                                onChange={(event) => setNumberOfPeople(event.target.value)}
-                                required>
-                                <option defaultValue="">Number of people</option>
-                                <option value="1">1</option>
-                                <option value="2">2</option>
-                                <option value="3">3</option>
-                                <option value="4">4</option>
-                                <option value="5">5</option>
-                                <option value="6">6</option>
-                                <option value="6">7 or more</option>
-
-                            </select></Paper>
+                                </select></Paper>
                     </Grid>
-                    <Grid item xs={3}>
-                        <Paper className={classes.paper}>xs=3</Paper>
-                    </Grid>
-                    <Grid item xs={3}>
-                        <Paper className={classes.paper}>xs=3</Paper>
-                    </Grid>
-                    <Grid item xs={3}>
-                        <Paper className={classes.paper}>xs=3</Paper>
-                    </Grid>
-                    <Grid item xs={3}>
-                        <Paper className={classes.paper}>xs=3</Paper>
-                    </Grid>
+                        <Grid item xs={12}>
+                            <Paper className={classes.paper}>
+                                <label for="time"> number of people able to attend: </label>
+                                <select
+                                    type="number"
+                                    name="number of people"
+                                    value={numberOfPeople}
+                                    onChange={(event) => setNumberOfPeople(event.target.value)}
+                                    required>
+                                    <option defaultValue="">Number of people</option>
+                                    <option value="1">1</option>
+                                    <option value="2">2</option>
+                                    <option value="3">3</option>
+                                    <option value="4">4</option>
+                                    <option value="5">5</option>
+                                    <option value="6">6</option>
+                                    <option value="6">7 or more</option>
+                                </select></Paper>
+                            <Paper className={classes.paper}>
+                                <button type="submit">Submit</button></Paper>
+                        </Grid>
                 </Grid>
             </div>
        </>
